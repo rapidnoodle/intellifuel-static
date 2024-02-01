5aa4f0ab7b1300a3a77cd243087c726d0aa253cf
Michael Marsico <michaelrmarsico@icloud.com>
2024-02-01 10:35:18 -0600

    Implement Basic HTML Markup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#A8A6A1",
+      },
+    },
   },
   plugins: [],
 }
-
