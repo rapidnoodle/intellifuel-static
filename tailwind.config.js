2ef36d80fff2ede07afb1f7d182333b0fb00ead1
Michael Marsico <michaelrmarsico@icloud.com>
2024-02-05 10:38:32 -0600

    Style Main Content and Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        dark: '#38383B',
         primary: "#A8A6A1",
+        'dark-primary': "#999793",
       },
       backgroundImage: {
-        'hero': "url('https://picsum.photos/1080/512')",
+        'hero': "url('/assets/intellifuel-banner.jpg')",
         'content': "url('https://picsum.photos/400')",
       }
     },
